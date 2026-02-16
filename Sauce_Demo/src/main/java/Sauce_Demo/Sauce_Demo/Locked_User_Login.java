57974a0e645c0061f53c7b0244d6258c6b35c167
jyoti <jyoti@10.0.0.169>
2026-02-16 11:41:20 -0800

    Initial commit

---
+++ Sauce_Demo/src/main/java/Sauce_Demo/Sauce_Demo/Locked_User_Login.java
@@ -0,0 +1,23 @@
+package Sauce_Demo.Sauce_Demo;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Locked_User_Login {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+	    WebDriver driver=new ChromeDriver();
+			driver.get("https://www.saucedemo.com/");
+			driver.manage().window().maximize();
+			Thread.sleep(2000);
+			driver.findElement(By.id("user-name")).sendKeys("locked_out_user");
+			Thread.sleep(5000);
+			driver.findElement(By.id("password")).sendKeys("secret");
+			Thread.sleep(5000);
+			driver.findElement(By.id("login-button")).click();
+			driver.close();	
+	}
+
+}
---
+++ Sauce_Demo/src/main/java/Sauce_Demo/Sauce_Demo/verify_ProductList.java
@@ -0,0 +1,36 @@
+package Sauce_Demo.Sauce_Demo;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class verify_ProductList {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		{
+		     WebDriver driver=new ChromeDriver();
+			
+			driver.get("https://www.saucedemo.com/");
+			driver.manage().window().maximize();
+			Thread.sleep(5000);
+			
+			driver.findElement(By.id("user-name")).sendKeys("standard_user");
+			Thread.sleep(5000);
+			driver.findElement(By.id("password")).sendKeys("secret_sauce");
+			Thread.sleep(5000);
+			driver.findElement(By.id("login-button")).click();
+
+			driver.findElement(By.id("react-burger-menu-btn")).click();
+			Thread.sleep(8000);
+			driver.findElement(By.xpath("//*[@id=\"inventory_sidebar_link\"]")).click();
+			Thread.sleep(8000);
+			driver.findElement(By.id("react-burger-cross-btn")).click();
+			
+			Thread.sleep(8000);
+			//driver.findElement(By.name("add-to-cart-sauce-labs-bike-light")).click();
+			//driver.close();
+	}
+
+}
+}
